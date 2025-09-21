a4da832cbcbe0c42d1ce53d7ffd068dd5589e657
BenBrudo <bbr@atolcd.com>
2025-09-21 11:57:55 +0200

    fix(about): update special characters for proper HTML encoding

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,14 +8,14 @@ const AboutUs: NextPage = () => (
     <div className="space-y-24">
       <div className="py-6">
         <h1 className="text-4xl font-semibold text-center text-white md:text-left">
-          Les Bénévoles du Silverado
+          Les B&eacute;n&eacute;voles du Silverado
         </h1>
       </div>
 
       <div className="space-y-6">
-        <h1 className="text-4xl font-bold text-gray-700">L'équipe des bénévoles.</h1>
+        <h1 className="text-4xl font-bold text-gray-700">L&apos;&eacute;quipe des b&eacute;n&eacute;voles.</h1>
         <div>
-          Coucou c'est nous !
+          Coucou c&apos;est nous !
         </div>
       </div>
     </div>
