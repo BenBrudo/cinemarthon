18adb7a5186b4fc43bec73a01ed4097e0e0c890e
BenBrudo <bbr@atolcd.com>
2025-10-15 21:59:05 +0200

    fix(footer): update footer layout and content for clarity and accessibility

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,14 +4,18 @@ import { Github, Twitter, Link } from "../components/Icons";
 
 export const Footer: React.FC = () => (
   <footer className="px-6 mt-24 text-gray-900 bg-center bg-cover shadow-md md:px-0 bg-gray-200/40">
-    <div className="mx-auto text-center py-14 md:w-3/12 md:px-0">
+    <div className="mx-auto text-center py-14 md:w-4/12 md:px-0">
       <div className="flex flex-col space-y-8">
         <div className="space-y-4">
           <Title>Le Silverado</Title>
 
-          <p className="opacity-50">
-            All rights are not reserved. This is an open-source project, the
-            source is available on GitHub. All content and images belong to{" "}
+          <div className="opacity-50">
+            <p>CINEMA LE SILVERADO - 10 RUE DES ECOLES, 16380 MARTHON / (ACCES PMR). </p>
+            <p>NORMAL : 5,5€ / REDUIT (demandeurs d’emploi, étudiants, -16 ans) : 4,5€</p>
+            <p>CINE MERCREDI ET CINEFAMILLE : 3€</p>
+            <p>Carte 5 entrées : 22,5€ soit 4,5€ la place </p> 
+            <p>Carte 10 entrées 45€ + 1 entrée gratuite soit 4,1€ la place.</p>
+            <p>All content and images belong to{" "}</p>
             <a
               href="https://www.themoviedb.org/"
               target="_blank"
@@ -21,7 +25,7 @@ export const Footer: React.FC = () => (
               TMDb
             </a>
             .
-          </p>
+          </div>
         </div>
 
         <div className="flex flex-wrap items-center justify-center gap-4">
