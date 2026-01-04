69f0cd9bda6acfba91deace426145dcd0c70598a
BenBrudo <bbr@atolcd.com>
2026-01-04 15:42:23 +0100

    fix(movie): title position and font size

--- src/components/Card/FeaturedMovie.tsx
+++ src/components/Card/FeaturedMovie.tsx
@@ -24,33 +24,36 @@ export const FeaturedMoviedCard: React.FC<{ movie: MovieWithScreening }> = ({
         />
 
         <div className="absolute inset-x-0 bottom-0 px-6 py-4 text-white bg-gradient-to-t from-black via-black to-transparent">
-          <div className="flex flex-col gap-4 md:flex-row md:items-end">
-            <div
-              className="flex-shrink-0 w-24 bg-center bg-cover rounded-md h-36"
-              style={{
-                backgroundImage: `url('${posterUrl}')`,
-              }}
-            />
+          <div className="flex flex-col gap-4">
+            <div className="text-lg font-bold leading-tight line-clamp-2">
+              {movie.title}
+            </div>
 
-            <div className="flex flex-col items-start gap-4">
-              <div className="flex flex-col items-start gap-2 overflow-hidden">
-                <div className="text-lg font-medium leading-tight line-clamp-2">
-                  {movie.title}
-                </div>
-                <div>
-                  {Math.floor(movie.runtime /60)}h {movie.runtime % 60}m
-                </div>
-              </div>
+            <div className="flex flex-col gap-4 md:flex-row md:items-end">
+              <div
+                className="flex-shrink-0 w-24 bg-center bg-cover rounded-md h-36"
+                style={{
+                  backgroundImage: `url('${posterUrl}')`,
+                }}
+              />
 
-              <div className="flex flex-wrap gap-2">
-                <span className="px-2 font-medium py-1 space-x-1 text-sm rounded-lg bg-white/10 backdrop-blur-sm">
-                  {new Date(movie.screening_date).toLocaleDateString('fr-FR', { weekday: 'short', day: 'numeric', month: 'short' })}
-                </span>
-                {movie.hours && (
-                  <span className="px-2 font-medium py-1 space-x-1 text-sm rounded-lg bg-blue-500/20 backdrop-blur-sm">
-                    🕐 {movie.hours}
+              <div className="flex flex-col items-start gap-4">
+                <div className="flex flex-wrap gap-2">
+                  <span className="px-2 font-medium py-1 space-x-1 text-base rounded-lg bg-white/10 backdrop-blur-sm">
+                    {new Date(movie.screening_date).toLocaleDateString('fr-FR', { weekday: 'short', day: 'numeric', month: 'short' })}
                   </span>
-                )}
+                  {movie.hours && (
+                    <span className="px-2 font-medium py-1 space-x-1 text-base rounded-lg bg-blue-500/20 backdrop-blur-sm">
+                      🕐 {movie.hours}
+                    </span>
+                  )}
+                </div>
+
+                <div className="flex flex-col items-start gap-2 overflow-hidden">
+                  <div>
+                    {Math.floor(movie.runtime /60)}h {movie.runtime % 60}m
+                  </div>
+                </div>
               </div>
             </div>
           </div>
